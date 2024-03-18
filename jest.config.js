3b9116e108ecb0f9c7ac84c10859fcb14b07234b
Helena Rasche <hexylena@galaxians.org>
2024-03-18 15:33:23 +0100

    whatever

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   transform: {'^.+\\.ts?$': 'ts-jest'},
   testEnvironment: 'node',
-  testRegex: '/test/.*\\.(test|spec)?\\.(ts|tsx)$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  testRegex: '/test/.*\\.(test|spec)?\\.ts$',
+  moduleFileExtensions: ['ts', 'json']
 };
