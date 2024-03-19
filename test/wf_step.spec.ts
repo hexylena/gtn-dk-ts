a213169d38637631bb2273afbbf482f020dd74f5
Helena Rasche <hexylena@galaxians.org>
2024-03-19 14:00:44 +0100

    test passes

--- test/wf_step.spec.ts
+++ test/wf_step.spec.ts
@@ -18,6 +18,43 @@ describe(get_input_tool_name, () => {
   })
 })
 
+const goseq_desc = `
+## Sub-step with **Column join**
+
+> <hands-on-title> Task description </hands-on-title>
+>
+> 1. {% tool [Column join](toolshed.g2.bx.psu.edu/repos/iuc/collection_column_join/collection_column_join/0.0.3) %} with the following parameters:
+>    - {% icon param-collection %} *"Tabular files"*: \`output\` (Input dataset collection)
+>    - *"Number of header lines in each input file"*: \`1\`
+>
+>    ***TODO***: *Check parameter descriptions*
+>
+>    ***TODO***: *Consider adding a comment or tip box*
+>
+>    > <comment-title> short description </comment-title>
+>    >
+>    > A comment about the tool or something else. This box can also be in the main text
+>    {: .comment}
+>
+{: .hands_on}
+
+***TODO***: *Consider adding a question to test the learners understanding of the previous exercise*
+
+> <question-title></question-title>
+>
+> 1. Question1?
+> 2. Question2?
+>
+> > <solution-title></solution-title>
+> >
+> > 1. Answer for question1
+> > 2. Answer for question2
+> >
+> {: .solution}
+>
+{: .question}
+`
+
 
 describe('fuck it we ball', () => {
   test('process_wf_step', async () => {
@@ -25,7 +62,13 @@ describe('fuck it we ball', () => {
     let flat = flatten_workflow_steps(wf.steps).map(x => x[1]);
     let tool_descs = await obtain_tool_descs(flat);
 
-    let out = process_wf_step(step["3"], tool_descs, flat)
-    expect(out).toBe("x")
+    // Check that tool descs looks right.
+    let goseq = tool_descs["toolshed.g2.bx.psu.edu/repos/iuc/goseq/goseq/1.50.0+galaxy0"];
+    expect(goseq['description']).toBe("tests for overrepresented gene categories")
+    expect(goseq['tool_shed_repository']['changeset_revision']).toBe(`602de62d995b`)
+
+    console.log(flat)
+    let out = process_wf_step(step, tool_descs, flat)
+    expect(out).toBe(goseq_desc)
   })
 })
